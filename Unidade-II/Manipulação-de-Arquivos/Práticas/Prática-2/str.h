ea64f725b57b211722359c43bcc830ac5fecf6b0
Maria Aparecida <m.aparecida.silva04@gmail.com>
2024-08-01 11:10:14 -0300

    Criado questões práticas

---
+++ Unidade-II/Manipulação-de-Arquivos/Práticas/Prática-2/str.h
@@ -0,0 +1,15 @@
+/*Funcoes oferecidas pelo modulo str.c*/
+
+/*funcao comprimento*/
+
+/*retorna o numero de caracteres da string passada como parametro*/
+
+int comprimento (char* str);
+
+/*funcao copia*/
+/*copia os caracteres da string orig (origem) para dest (destino)*/
+void copia(char* dest, char* orig);
+
+/*funcao concatena*/
+/*concatena a string orig (origem) na string dest (destino)*/
+void concatena (char* dest, char* orig);
